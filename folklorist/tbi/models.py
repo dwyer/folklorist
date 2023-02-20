b15d1c0931f326bf05893fac8503c22b0647eb0d
Casey Dwyer <caseydwyer@gmail.com>
2023-02-19 21:32:32 -0800

    Update database.

--- folklorist/tbi/models.py
+++ folklorist/tbi/models.py
@@ -5,24 +5,40 @@ from django.contrib.postgres.fields import ArrayField
 class IndexableCharField(models.CharField):
 
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('max_length', 254)
+        kwargs.setdefault('max_length', 255)
         super().__init__(*args, **kwargs)
 
 
 class Ballad(models.Model):
     name = IndexableCharField()
     description = IndexableCharField()
-    author = IndexableCharField()
+    author = IndexableCharField(max_length=300)
     earliest_date = IndexableCharField()
     long_description = models.TextField()
-    keywords = ArrayField(IndexableCharField())
-    historical_references = ArrayField(models.TextField())
+    keywords = ArrayField(IndexableCharField(), null=True)
+    historical_references = ArrayField(models.TextField(), null=True)
     found_in = IndexableCharField()
-    references = ArrayField(models.TextField())
-    recordings = ArrayField(models.TextField())
-    broadsides = ArrayField(models.TextField())
-    cross_references = ArrayField(IndexableCharField())
-    same_tune = ArrayField(models.TextField())
-    alternate_titles = ArrayField(IndexableCharField())
+    references = ArrayField(models.TextField(), null=True)
+    recordings = ArrayField(models.TextField(), null=True)
+    broadsides = ArrayField(models.TextField(), null=True)
+    cross_references = ArrayField(IndexableCharField(), null=True)
+    same_tune = ArrayField(models.TextField(), null=True)
+    alternate_titles = ArrayField(IndexableCharField(), null=True)
     notes = models.TextField()
     file = IndexableCharField()
+
+
+class BalladIndex(models.Model):
+    name = IndexableCharField(blank=False)
+    index = ArrayField(IndexableCharField())
+
+
+class BalladName(models.Model):
+    name = IndexableCharField(blank=False)
+    title = IndexableCharField(blank=False)
+
+
+class SuppTradFile(models.Model):
+    name = IndexableCharField(blank=False)
+    text = models.TextField(blank=False)
+    file = IndexableCharField(blank=False)
