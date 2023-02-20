d15fc69c3ee2a1f53b5cb2129e06874253b297f9
Casey Dwyer <caseydwyer@gmail.com>
2023-02-19 20:01:29 -0800

    Add and use tbi.context_processors.

---
+++ folklorist/tbi/context_processors.py
@@ -0,0 +1,5 @@
+from ballads.hacks import alphabet
+
+
+def alphanum_nav(request):
+    return {'vols': alphabet()}
--- folklorist/tbi/views.py
+++ folklorist/tbi/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-from ballads.hacks import alphabet
-
 
 
 def index_view(request):
-    context = {'vols': alphabet()}
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
