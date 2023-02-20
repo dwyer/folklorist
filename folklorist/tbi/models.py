2a74d1bb729cca6d0592ebefc150a4d8e68f5755
Casey Dwyer <caseydwyer@gmail.com>
2023-02-19 19:49:43 -0800

    Add tbi.models.BalladIndex.

--- folklorist/tbi/models.py
+++ folklorist/tbi/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class IndexableCharField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 254)
+        super().__init__(*args, **kwargs)
+
+
+class BalladIndex(models.Model):
+    name = IndexableCharField()
+    description = IndexableCharField()
+    author = IndexableCharField()
+    earliest_date = IndexableCharField()
+    long_description = models.TextField()
+    keywords = ArrayField(IndexableCharField())
+    historical_references = ArrayField(models.TextField())
+    found_in = IndexableCharField()
+    references = ArrayField(models.TextField())
+    recordings = ArrayField(models.TextField())
+    broadsides = ArrayField(models.TextField())
+    cross_references = ArrayField(IndexableCharField())
+    same_tune = ArrayField(models.TextField())
+    alternate_titles = ArrayField(IndexableCharField())
+    notes = models.TextField()
+    file = IndexableCharField()
