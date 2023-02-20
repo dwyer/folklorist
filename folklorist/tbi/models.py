3d07491befcbf48379f1d53514f0a65a27e77491
Casey Dwyer <caseydwyer@gmail.com>
2023-02-19 21:44:42 -0800

    Fix search page.

--- folklorist/tbi/models.py
+++ folklorist/tbi/models.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
@@ -26,6 +28,22 @@ class Ballad(models.Model):
     alternate_titles = ArrayField(IndexableCharField(), null=True)
     notes = models.TextField()
     file = IndexableCharField()
+
+    def title(self):
+        title = self.name
+        title = re.sub(r' \[.*\]$', '', title)
+        # articles
+        articles = '|'.join(['A', 'An', 'El', 'Le', 'Ta', 'The'])
+        title = re.sub(r'^(.*), (%s)$' % articles, r'\2 \1', title)
+        title = re.sub(r'^(.*), (%s) (\(.*\))$' % articles, r'\2 \1 \3', title)
+        return title
+
+    def urlencoded_title(self):
+        return (self.title().replace(' ', '_').replace('"', '%22')
+                .replace(';', '%3B'))
+
+    def url(self):
+        return '/song/%s' % self.urlencoded_title()
 
 
 class BalladIndex(models.Model):
